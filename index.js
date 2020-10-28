e7491985656df5e05113d01a6a92bac95eac7b67
dimaskurniawan8 <dimaskurniawan60@gmail.com>
2020-10-28 16:20:57 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,88 @@
+// ===============================>>var dua variabel<<==============================//
+
+
+var angkaPertamaKurang = 2000
+var angkaKeduaKurang = 1
+var hasilKurang = angkaPertamaKurang - angkaKeduaKurang
+console.log('hasil dari ' + angkaPertamaKurang + ' dikurang ' + angkaKeduaKurang + ' adalah ' + hasilKurang)
+
+var angkaPertamaBagi = 18
+var angkaKeduaBagi = 2
+var hasilBagi = angkaPertamaBagi / angkaKeduaBagi
+console.log('hasil dari ' + angkaPertamaBagi + ' dibagi ' + angkaKeduaBagi + ' adalah ' + hasilBagi)
+
+var angkaPertamaKali = 4
+var angkaKeduaKali = 2
+var hasilKali = angkaPertamaKali * angkaKeduaKali
+console.log('hasil dari ' + angkaPertamaKali + ' dikali ' + angkaKeduaKali + ' adalah ' + hasilKali)
+
+
+// === === === === === === === === =>
+
+// ==========================>>menghitung luas bangunan<<===============================//
+
+var panjangPersegiPanjang = 20
+var lebarPersegiPanjang = 8
+var hasilnya = panjangPersegiPanjang * lebarPersegiPanjang
+console.log('Hasil dari panjang ' + panjangPersegiPanjang + ' dikali lebar ' + lebarPersegiPanjang + ' adalah ' + hasilnya)
+
+var persegiSisi = 95
+var sisiPersegi = 95
+var hasilnye = persegiSisi * sisiPersegi
+console.log('Hasil dari sisi ' + persegiSisi + ' dikali sisi ' + sisiPersegi + ' adalah ' + hasilnye)
+
+var segitiga = 0.5
+var segitigaAlas = 15
+var segitigaTinggi = 20
+var hasilnyo = segitiga * segitigaAlas * segitigaTinggi
+console.log('Hasil dari ½ ' + segitiga + ' dikali alas ' + segitigaAlas + ' dikali tinggi ' + segitigaTinggi + ' adalah ' + hasilnyo)
+
+var kubus = 6
+var kubusSisi = 20
+var kubusSisi2 = 20
+var hisilnyi = kubus * kubusSisi * kubusSisi2
+console.log('Hasil dari ' + kubus + ' dikali Sisi ' + kubusSisi + ' dikali Sisi ' + kubusSisi2 + ' adalah ' + hisilnyi)
+
+var lingkaran = 3.14
+var lingkaranJari = 80
+var lingkaranJariJari = 80
+var husulnya = lingkaran * lingkaranJari * lingkaranJariJari
+console.log('Hasil dari ' + lingkaran + ' dikali Jari-jari ' + lingkaranJari + ' dikali Jari-jari ' + lingkaranJariJari + ' adalah ' + husulnya)
+
+//======================================>>
+
+// =======================>>Belajar if else<<=================================//
+
+var benar = true
+if (benar) {
+    console.log('Aku Suka Payudara')
+} else {
+    console.log('Ga Suka yang tepos')
+}
+
+//======================================>>
+
+
+//=========================>>Belajar swicth<<=================================//
+
+var satu = 4
+
+switch (satu) {
+    case 1:
+        console.log("Ukuran payudara 10-12 cm")
+        break;
+    case 2:
+        console.log("Ukuran payudara 13-14 cm")
+        break;
+    case 3:
+        console.log("Ukuran payudara 15-17 cm")
+        break;
+    case 4:
+        console.log("Ukuran payudara 18-19 cm")
+        break;
+    default:
+        console.log("Mungkin dia tepos")
+}
+
+
+//=======================================>>
